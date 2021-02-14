046caa17d3c0164832199a32e3d972750ab9acf6
ahmed <a.eldiasty@indexgroup.net>
2021-02-14 13:59:07 +0200

    Finished

--- src/provider/ContentHOC.tsx
+++ src/provider/ContentHOC.tsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
 import { ContentHeader, ContentList } from '../components';
-import { DataApi } from '../services/DataApi';
 import { useSelector } from 'react-redux';
 import { completeUrls } from '../urls/urls';
+import axios from 'axios';
+import {FETCH_DATA} from '../actions/types';
+import store from '../store/store';
+import perfix from '../utils/perfix';
+import { DataApi } from '../services/DataApi';
 
 interface Props {
   startPressed: boolean;
@@ -10,10 +14,56 @@ interface Props {
 
 export default (props: Props) => {
   const fetchedData: any = useSelector((state) => state);
+  
+
+  const fetchData = async (list: string[]) => {
+    let data = store.getState().fetchData.data;
+    const run: (requests: string[], responses: any[], index: number) => any = async (
+      requests,
+      responses,
+      index,
+    ) => {
+      if (requests.length === 0) {
+        return responses;
+      }
+      const currentRequest = requests[0];
+      const newRequests = requests.filter((item, index) => {
+        return index != 0;
+      });
+      try {
+        const res = await DataApi(currentRequest);
+        data[index] = {
+          name: currentRequest.slice(12),
+          response: res.data.length,
+          index,
+          logo: `${perfix.favPerfix}${currentRequest.slice(12)}`,
+          status: 200,
+        };
+      } catch (e) {
+        data[index] = {
+          name: currentRequest.slice(12),
+          index,
+          status: 404,
+        };
+      } finally {
+        store.dispatch({
+          type: FETCH_DATA,
+          payload: {
+            data,
+          },
+        });
+        return run(newRequests, responses, ++index);
+      }
+    };
+    const result = run(list, [], 0);
+    return result;
+  };
+  
 
   useEffect(() => {
     async function getData() {
-      await DataApi(completeUrls);
+      await fetchData(completeUrls);
+      
     }
     if (props.startPressed) getData();
   }, [props.startPressed]);
--- src/services/DataApi.ts
+++ src/services/DataApi.ts
@@ -1,47 +1,10 @@
 import axios from 'axios';
-import {FETCH_DATA} from '../actions/types';
-import store from '../store/store';
-import perfix from '../utils/perfix';
 
-export const DataApi = async (list: string[]) => {
-  let data = store.getState().fetchData.data;
-  const run: (requests: string[], responses: any[], index: number) => any = async (
-    requests,
-    responses,
-    index,
-  ) => {
-    if (requests.length === 0) {
-      return responses;
-    }
-    const currentRequest = requests[0];
-    const newRequests = requests.filter((item, index) => {
-      return index != 0;
-    });
-    try {
-      const res = await axios.get(currentRequest);
-      data[index] = {
-        name: currentRequest.slice(12),
-        response: res.data.length,
-        index,
-        logo: `${perfix.favPerfix}${currentRequest.slice(12)}`,
-        status: 200,
-      };
-    } catch (e) {
-      data[index] = {
-        name: currentRequest.slice(12),
-        index,
-        status: 404,
-      };
-    } finally {
-      store.dispatch({
-        type: FETCH_DATA,
-        payload: {
-          data,
-        },
-      });
-      return run(newRequests, responses, ++index);
-    }
-  };
-  const result = run(list, [], 0);
-  return result;
+export const DataApi = async (url: string) => {
+  try {
+    const res = await axios.get(url);
+    return res;
+  } catch (e) {
+    throw 'Data not resolved';
+  }
 };
